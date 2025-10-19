ee3cf5bc45f8f42951e3ad0b756490de2bdc500a
ShiSuYa <59110564+ShiSuYa@users.noreply.github.com>
2025-10-19 13:46:39 +0300

    Update notes.py

--- app/api/notes.py
+++ app/api/notes.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from app.services.note_service import NoteService
 from app.infrastructure.repository import InMemoryNoteRepository
+import logging
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter()
 service = NoteService(InMemoryNoteRepository())
@@ -18,12 +21,15 @@ class NoteResponse(BaseModel):
 
 @router.post("/notes", response_model=NoteResponse)
 def create_note(note: NoteCreate):
+    logger.info(f"API: запрос на создание заметки '{note.title}'")
     created = service.create_note(note.title, note.content)
     return NoteResponse(**created.__dict__)
 
 @router.get("/notes/{note_id}", response_model=NoteResponse)
 def get_note(note_id: int):
+    logger.info(f"API: запрос на получение заметки ID={note_id}")
     found = service.get_note(note_id)
     if not found:
+        logger.warning(f"API: заметка ID={note_id} не найдена")
         raise HTTPException(status_code=404, detail="Note not found")
     return NoteResponse(**found.__dict__)
