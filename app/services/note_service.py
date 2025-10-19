d34b18a2533b0a011a3e6c11098faf5f3c972714
ShiSuYa <59110564+ShiSuYa@users.noreply.github.com>
2025-10-19 13:45:55 +0300

    Update note_service.py

--- app/services/note_service.py
+++ app/services/note_service.py
@@ -1,12 +1,20 @@
+import logging
 from app.domain.models import Note
 from app.infrastructure.repository import InMemoryNoteRepository
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s")
 
 class NoteService:
     def __init__(self, repo: InMemoryNoteRepository):
         self.repo = repo
+        self.create_counter = 0
 
     def create_note(self, title: str, content: str) -> Note:
+        self.create_counter += 1
+        logger.info(f"Создание заметки: {title}")
         return self.repo.create(title, content)
 
     def get_note(self, note_id: int) -> Note | None:
+        logger.info(f"Получение заметки с ID: {note_id}")
         return self.repo.get(note_id)
