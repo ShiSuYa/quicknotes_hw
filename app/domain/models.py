528cf2201fa157373eb3012da96d79dd6b9e8a19
ShiSuYa <59110564+ShiSuYa@users.noreply.github.com>
2025-11-22 13:05:26 +0300

    Update models.py

--- app/domain/models.py
+++ app/domain/models.py
@@ -1,16 +1,12 @@
 from datetime import datetime
 from pydantic import BaseModel, model_serializer
 
-class NoteCreate(BaseModel):
-    title: str
-    content: str
-
 class NoteResponse(BaseModel):
     id: int
     title: str
     content: str
     created_at: datetime
 
-    @model_serializer(mode="json")
+    @model_serializer(mode="wrap")  # вместо "json"
     def serialize_created_at(self, value: datetime) -> str:
         return value.isoformat()
